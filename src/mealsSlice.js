e339c60ae17ef431230ee8ff8427aa61000ca350
xiatiantian24 <you@example.com>
2024-07-13 02:03:30 +0800

    fin

--- src/mealsSlice.js
+++ src/mealsSlice.js
@@ -4,10 +4,14 @@ import { createSlice } from '@reduxjs/toolkit';
 export const mealsSlice = createSlice({
   name: 'meals',
   initialState: [
-   
+    { name: 'Breakfast', cost: 50, selected: false },
+    { name: 'High Tea', cost: 25, selected: false },
+    { name: 'Lunch', cost: 65, selected: false },
+    { name: 'Dinner', cost: 70, selected: false },
   ],
   reducers: {
     toggleMealSelection: (state, action) => {
+      state[action.payload].selected = !state[action.payload].selected; //takes the current state and an action object, using action.payload to identify the item to update. It then switches the selected status of that item from true to false or vice versa.
     },
   },
 });
--- src/store.js
+++ src/store.js
@@ -1,9 +1,14 @@
 // store.js
 import { configureStore } from '@reduxjs/toolkit';
 import venueReducer from './venueSlice';
+import avReducer from './avSlice';
+import mealsReducer from './mealsSlice';
 
 export default configureStore({
   reducer: {
     venue: venueReducer,
+    av: avReducer,
+    meals: mealsReducer,
   },
 });
+//creates a global Redux store so all components in the application can access the state managed by the venueReducer().
